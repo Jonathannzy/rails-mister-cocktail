0e70db5df65c0c3a8d1b066b7fe57320bf3901db
(Jonathan Ng) <nzyjonathan@gmail.com>
2020-05-27 21:52:59 +0800

    fixed models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,14 +1,4 @@
 class Ingredient < ApplicationRecord
   has_many :doses
   validates :name, presence: true, uniqueness: true
-  before_destroy :check_for_cocktail
-
-  private
-
-  def check_for_cocktail
-    if Cocktail.count > 0
-      errors.add_to_base("can't delete while used by cocktail")
-      return false
-    end
-   end
 end
